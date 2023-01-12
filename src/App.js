cdb25fff9a424e27b98f3a9b7fb1eab2e689fac5
Maxim <kosmos-frontend@bk.ru>
2023-01-12 22:13:09 +0300

    second project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Footer } from "./Components/Footer";
+import { Header } from "./Components/Header";
+import { Movies } from "./Components/Movies";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component {
+
+  state = {
+    movie: [],
+    isLoaded: false
+  }
+
+  componentDidMount() {
+    fetch('http://www.omdbapi.com/?apikey=f426164a&s=marvel')
+      .then(res => res.json())
+      .then(result => {
+        this.setState({
+          movie: result.Search
+        });
+      })
+  }
+
+  render() {
+    const {movie} = this.state
+
+    return (
+      <>
+        <Header />
+        {movie.length ? <Movies films={movie} /> : <h5>...Loading</h5>}
+        <Footer />
+      </>
+    )
+  }
 }
 
 export default App;
